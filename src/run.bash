0c409bf2254b996671ac608c5a54b6aa7f7d7366
Yi Tian Xu <yxu219@cim.mcgill.ca>
2018-10-07 15:52:10 -0400

    added more jobs

--- src/run.bash
+++ src/run.bash
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+python train.py -m HH_RNN -ld 1024 -rep --supervised -iter 500
+
 loss_func=( "mean_squared_error" "mean_absolute_error" );
 optimizers=( "Nadam()" "Adam(lr=0.002)" "Adam()" "Nadam(lr=0.001)" "RMSprop()" );
 iters=200;
@@ -17,4 +19,6 @@ do
 		python train.py -m HH_RNN -t 20 -to 10 -ld $ld -lf $loss -opt $opt -hs 9 19 -iter $iters;
 	done
 done;
+
+#python train.py -m Seq2Seq -ld 1024 --supervised
 
