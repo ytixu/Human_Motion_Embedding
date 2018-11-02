ede4a315113fee1c83c33bd06a176e6c58f21936
Yi Tian Xu <yxu219@cim.mcgill.ca>
2018-11-02 14:22:18 -0400

    changed C_RNN

--- src/models/C_RNN.py
+++ src/models/C_RNN.py
@@ -18,8 +18,10 @@ class C_RNN(abs_model.AbstractModel):
 		self.output_dim = self.name_dim
 
 		inputs = K_layer.Input(shape=(self.timesteps, self.input_dim))
-		encoded = abs_model.RNN_UNIT(self.latent_dim)(inputs)
-		decoded = K_layer.Dense(self.latent_dim/2, activation='relu')(encoded)
+		#encoded = abs_model.RNN_UNIT(self.latent_dim, return_sequences=True)(inputs)
+		decoded = K_layer.Flatten()(inputs)
+		decoded = K_layer.Dense(self.latent_dim, activation='relu')(decoded)
+		decoded = K_layer.Dense(self.latent_dim/2, activation='relu')(decoded)
 		decoded = K_layer.Dense(self.output_dim, activation='sigmoid')(decoded)
 		decoded = K_layer.Lambda(lambda x: K.tf.nn.softmax(x))(decoded)
 		output = K_layer.RepeatVector(self.timesteps)(decoded)
@@ -41,3 +43,4 @@ class C_RNN(abs_model.AbstractModel):
 
 	def classify(self, x, **kwargs):
 		return self.model.predict(x)
+
